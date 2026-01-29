61c102f7b3ebf00a0a6a8e569b129461b1081f94
Allison Liao <allison.y.liao@gmail.com>
2026-01-29 15:08:56 -0500

    copy over code

--- 01/myenv.c
+++ 01/myenv.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char** argv, char** envp)
-{
+int main(int argc, char** argv, char** envp) {
+  while (*envp) {
+    char path[] = "PATH=";
+    char str[6];
+    strncpy(str, *envp, 5);
+    str[5] = '\0';
+
+    if (strcmp(str, path) == 0) {
+      printf("%s\n", *envp);
+    }
+    *envp++;
+  }
 }
--- 01/outerr.c
+++ 01/outerr.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+  for (int i = 0; i < 10; i++) {
+    if (i % 2 == 0) {
+      // cout << i;
+      fprintf(stdout, "%d\n", i);
+    } else {
+      // cerr << i;
+      fprintf(stderr, "%d\n", i);
+    }
+  }
 }
