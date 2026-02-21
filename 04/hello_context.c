3e44921ef3f3f8d2922bb3cd6e1da87f74d63c20
Allison Liao <allison.y.liao@gmail.com>
2026-02-20 19:51:31 -0500

    add code

--- 04/hello_context.c
+++ 04/hello_context.c
@@ -1,3 +1,4 @@
+// #define _XOPEN_SOURCE 600  // or 500
 #include <ucontext.h>
 #include <sys/types.h>
 #include <signal.h>
@@ -12,7 +13,7 @@ void f(){
 
 int main(int argc, char * argv[]){
 
-  ucontext_t uc;
+  ucontext_t uc, uc_main;
   getcontext(&uc); // initialize context
 
   // setup stack and signal handling
@@ -21,11 +22,12 @@ int main(int argc, char * argv[]){
   uc.uc_stack.ss_size = STACKSIZE;
   uc.uc_stack.ss_flags = SS_DISABLE;
   sigemptyset(&(uc.uc_sigmask));
-  uc.uc_link = NULL;
+  uc.uc_link = &uc_main;
 
   makecontext(&uc, f, 0); // associate context with f()
-  setcontext(&uc); // "calls" f(); doesn't return if successful
-  perror("setcontext"); //setcontext() doesn’t return on success
+  // setcontext(&uc); // "calls" f(); doesn't return if successful
+  swapcontext(&uc_main, &uc);
+  // perror("setcontext"); //setcontext() doesn’t return on success
   return 0;
 }
 
