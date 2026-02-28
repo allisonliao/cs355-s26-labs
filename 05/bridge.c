f4770fc1248831ebeba4613deb83a4b6633b3ff4
Allison Liao <allison.y.liao@gmail.com>
2026-02-28 16:56:08 -0500

    lab 5

--- 05/bridge.c
+++ 05/bridge.c
@@ -1,19 +1,19 @@
+#include <assert.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <assert.h>
 
-typedef enum { OUTLOOK, TOWN} location_t;
-const char* location_string[2] = { "Outlook", "Town" };
+typedef enum { OUTLOOK, TOWN } location_t;
+const char* location_string[2] = {"Outlook", "Town"};
 
 struct bridge_t {
   int num_on_bridge[2];
   location_t direction;
   pthread_mutex_t lock;
   pthread_cond_t free;
-} bridge; // global variable for bridge
+} bridge;  // global variable for bridge
 
 struct thread_data {
   int id;
@@ -21,41 +21,50 @@ struct thread_data {
   location_t start_destination;
 };
 
-void *Move(void *args){
-  struct thread_data* data = (struct thread_data*) args;
+void* Move(void* args) {
+  struct thread_data* data = (struct thread_data*)args;
   int id = data->id;
   location_t destination = data->start_destination;
 
   for (int i = 0; i < data->num_trips; i++) {
-    
-    //cross bridge
+    pthread_mutex_lock(&bridge.lock);
+
+    while (bridge.num_on_bridge[(destination + 1) % 2] > 0) {
+      pthread_cond_wait(&bridge.free, &bridge.lock);
+    }
+
     bridge.direction = destination;
-    printf("Tourist %d takes their %d/%d trip towards %s\n", 
-      id, i, data->num_trips, location_string[destination]);
-
     bridge.num_on_bridge[destination]++;
-    // Assert that no one is on the bridge, going the opposite way
-    assert(bridge.num_on_bridge[(bridge.direction+1)%2] == 0);
 
-    sleep(rand()/RAND_MAX);
+    printf("Tourist %d takes their %d/%d trip towards %s\n", id, i,
+           data->num_trips, location_string[destination]);
+
+    assert(bridge.num_on_bridge[(bridge.direction + 1) % 2] == 0);
+
+    pthread_mutex_unlock(&bridge.lock);
+
+    sleep(rand() % 2);
+
+    pthread_mutex_lock(&bridge.lock);
 
     bridge.num_on_bridge[destination]--;
-    // Assert that number on bridge never goes below zero
-    // Assert that no one is on the bridge, going the opposite way
-    assert(bridge.num_on_bridge[destination] >= 0); 
-    assert(bridge.num_on_bridge[(bridge.direction+1)%2] == 0); 
 
-    // spend time at the new location and then change destination
-    sleep(rand()/RAND_MAX);
-    location_t current = destination;
-    destination = (location_t) ((destination + 1) % 2);
+    assert(bridge.num_on_bridge[destination] >= 0);
+    assert(bridge.num_on_bridge[(bridge.direction + 1) % 2] == 0);
+
+    pthread_cond_broadcast(&bridge.free);
+
+    pthread_mutex_unlock(&bridge.lock);
+
+    sleep(rand() % 2);
+
+    destination = (location_t)((destination + 1) % 2);
   }
 
   return NULL;
 }
 
 int main(int argc, char** argv) {
-
   if (argc != 3) {
     printf("usage: %s <NumThreads> <NumTrips>\n", argv[0]);
     exit(0);
@@ -67,15 +76,17 @@ int main(int argc, char** argv) {
   bridge.direction = OUTLOOK;
   bridge.num_on_bridge[OUTLOOK] = 0;
   bridge.num_on_bridge[TOWN] = 0;
-  pthread_mutex_init(&(bridge.lock), NULL);
-  pthread_cond_init(&(bridge.free), NULL);
+  pthread_mutex_init(&bridge.lock, NULL);
+  pthread_cond_init(&bridge.free, NULL);
 
-  pthread_t* threads = (pthread_t*) malloc(sizeof(pthread_t) * N);
-  thread_data* data = (thread_data*) malloc(sizeof(thread_data) * N);
+  pthread_t* threads = (pthread_t*)malloc(sizeof(pthread_t) * N);
+  struct thread_data* data =
+      (struct thread_data*)malloc(sizeof(struct thread_data) * N);
+
   for (int i = 0; i < N; i++) {
     data[i].id = i;
     data[i].num_trips = ntrips;
-    data[i].start_destination = i < (N+1)/2? OUTLOOK: TOWN;
+    data[i].start_destination = i < (N + 1) / 2 ? OUTLOOK : TOWN;
     pthread_create(&threads[i], NULL, Move, &data[i]);
   }
 
@@ -89,4 +100,3 @@ int main(int argc, char** argv) {
   pthread_cond_destroy(&bridge.free);
   return 0;
 }
-
--- 05/dining-philosophers.c
+++ 05/dining-philosophers.c
@@ -1,52 +1,57 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
 #include <unistd.h>
 #define N 5
 #define ROUNDS 100
 
-pthread_mutex_t mutex[N]; // one per fork
+pthread_mutex_t mutex[N];  // one per fork
 struct thread_data {
   int id;
 };
 
-void *Philosophize(void *args) {
-  struct thread_data* data = (struct thread_data*) args;
-  
+void* Philosophize(void* args) {
+  struct thread_data* data = (struct thread_data*)args;
+
   printf("Starting thread %d\n", data->id);
   for (int i = 0; i < ROUNDS; i++) {
-    float duration = 0.01; 
+    float duration = 0.01;
 
     int decision = rand() % 2;
     if (decision == 0) {
       printf("%d is thinking\n", data->id);
       sleep(duration);
-    }
-    else {
+    } else {
       printf("%d is hungry\n", data->id);
 
-      int leftFork = data->id; 
-      int rightFork = (data->id + 1) % N; 
+      int leftFork = data->id;
+      int rightFork = (data->id + 1) % N;
 
-      pthread_mutex_lock(&mutex[leftFork]); 
-      pthread_mutex_lock(&mutex[rightFork]);
+      int firstFork = (leftFork < rightFork) ? leftFork : rightFork;
+      int secondFork = (leftFork < rightFork) ? rightFork : leftFork;
+
+      // pthread_mutex_lock(&mutex[leftFork]);
+      // pthread_mutex_lock(&mutex[rightFork]);
+      pthread_mutex_lock(&mutex[firstFork]); 
+      pthread_mutex_lock(&mutex[secondFork]);
       printf("%d is eating\n", data->id);
       sleep(duration);
-      pthread_mutex_unlock(&mutex[leftFork]);
-      pthread_mutex_unlock(&mutex[rightFork]);
+      // pthread_mutex_unlock(&mutex[leftFork]);
+      // pthread_mutex_unlock(&mutex[rightFork]);
+      pthread_mutex_unlock(&mutex[firstFork]); 
+      pthread_mutex_unlock(&mutex[secondFork]);
     }
   }
   return NULL;
 }
 
 int main() {
-
   struct thread_data data[N];
   for (int i = 0; i < N; i++) {
     pthread_mutex_init(&(mutex[i]), NULL);
-    data[i].id = i; 
-  } 
+    data[i].id = i;
+  }
 
   pthread_t threads[N];
 
@@ -63,5 +68,3 @@ int main() {
   }
   return 0;
 }
-
-
