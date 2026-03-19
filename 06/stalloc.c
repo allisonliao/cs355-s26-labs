1ff019d2d6dc7872fb4761acb94a296ced5ae51a
Ally Liao <aliao@brynmawr.edu>
2026-03-19 15:34:56 -0400

    add stalloc.c

--- 06/stalloc.c
+++ 06/stalloc.c
@@ -9,13 +9,18 @@ static char* allocp = allocbuf;  // a pointer to the next free block in memory
 
 char* stalloc(int n) 
 {
-  // implement me
-  return 0;
+  if (n > ALLOCSIZE) return 0;
+  allocp = allocp + n;
+  return allocp-n;
 }
 
 void stfree(char* p)
 {
-  // implement me
+  for (int i = 0; (i + p) < allocp; i++) {
+    char* temp = i+p;
+    *temp = NULL;
+  }
+  allocp = p;
 }
 
 int main()
@@ -25,16 +30,16 @@ int main()
   assert(allocp == (allocbuf + 200));
 
   char* b2 = stalloc(300);
-  assert(b2 == 0); // todo: update tests
-  assert(allocp == 0);
+  assert(b2 == (allocbuf+200));
+  assert(allocp == (allocbuf+500));
 
   stfree(b2);
-  assert(b1 == 0);
-  assert(allocp == 0);
+  assert(b1 == allocbuf);
+  assert(allocp == (allocbuf+200));
 
   char* b3 = stalloc(1000);
-  assert(b3 == 0);
-  assert(allocp == 0);
+  assert(b3 == (allocbuf+200));
+  assert(allocp == (allocbuf+1200));
 
   char* b4 = stalloc(0);
   assert(b4 == allocp);
